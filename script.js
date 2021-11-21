cf6f7f23a5d33efc5070ae27c447f004fd89e96f
Ardak102 <87607420+Ardak102@users.noreply.github.com>
2021-11-21 16:37:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const newYears = '28 Feb 2022'; // Время до нового года
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('mins');
+const secondsEl = document.getElementById('sec');
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+}
+
+// Initial call
+countdown();
+
+setInterval(countdown, 1000);
